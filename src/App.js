6025010a6742062c5f0112c9fd5d4afd1aa260b6
Ao Liu <aoliu@Aos-MacBook-Pro.local>
2020-01-22 14:41:37 -0800

    namepicker

--- src/App.js
+++ src/App.js
@@ -1,55 +1,83 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import './App.css'
+import NamePicker from './NamePicker'
+import {db} from './db'
+
+
 
 function App() {
-  var [outputs, setOutputs] = useState([])
+  const [messages, setMessages] = useState([])
+  const [name, setName] = useState('Augustina')
+
+  useEffect(()=>{
+    db.listen({
+      receive: m=> {
+        setMessages(current=> [m, ...current])
+      },
+      remove: id=> {
+        setMessages(current=> [...current].filter(m => m.id !== id))
+      },
+    })
+  },[])
 
   return <main>
 
     <header> 
-      <img className="logo"
-        src="https://previews.123rf.com/images/sabelskaya/sabelskaya1704/sabelskaya170400204/75484604-red-and-green-logo-template-with-stylized-watermelon-slice-with-two-bites-vector-illustration-isolat.jpg"
-      />
-      Chatter Box 
+      <div className="logo-wrap">
+        <img className="logo"
+          alt="logo"
+          src="https://dcassetcdn.com/design_img/2738551/659842/659842_14945486_2738551_45d1fa94_image.jpg" 
+        />
+        Chatterbox
+      </div>
+      <NamePicker onSave={name=>{}} />
     </header>
-    <div className="output-container">
-      {outputs.map(output=> <TextOutput text={output}/>)}
+
+    <div className="messages">
+      {messages.map((m,i)=>{
+        return <div key={i} className="message-wrap">
+          <div className="message">{m.text}</div>
+        </div>
+      })}
     </div>
-    {/* message */}
 
-    <TextInput onSend={newOutput=>setOutputs(prevState=>[
-      ...prevState, newOutput
-    ])} /> {/*custom element*/}
+    <TextInput onSend={(text)=> {
+      db.send({
+        text,name,ts: new Date(),
+      })
+      /*setMessages([text, ...messages])
+      console.log(messages)*/
+    }} />
 
+    
   </main>
 }
 
+
 function TextInput(props){
-  var [text, setText] = useState('')
-
-  return (
-    <div className="text-input">
-      <input value={text} className="input"
-        placeholder="write your message"
-        onChange={e=> setText(e.target.value)}
-      />
-      <button onClick ={()=>{ 
-        props.onSend(text)
-        setText('')
-      }}>
-        ↑
-      </button>
-    </div>
-  );
+  var [text, setText] = useState('') 
+  return <div className="text-input-wrap">
+    <input 
+      value={text} 
+      className="text-input"
+      placeholder="write your message"
+      onChange={e=> setText(e.target.value)}
+      onKeyPress={e=> {
+        if(e.key==='Enter') {
+          if(text) props.onSend(text)
+          setText('')
+        }
+      }}
+    />
+    <button onClick={()=> {
+      if(text) props.onSend(text)
+        setText('')}
+      } className="button"
+      disabled={!text}>
+      ↑
+    </button>
+  </div>
 }
 
-function TextOutput(props){
-  return(
-    <div className="text-output">
-      {props.text}
-    </div>
-  )
-}
+export default App
 
-
-export default App;
