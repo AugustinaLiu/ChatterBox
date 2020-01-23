95bdcbeedb68b940a2810fa0b9eb0d49b04f6a5b
Ao Liu <aoliu@Aos-MacBook-Pro.local>
2020-01-22 21:07:23 -0800

    namepickerV2

--- src/NamePicker.js
+++ src/NamePicker.js
@@ -3,30 +3,31 @@ import { FiEdit, FiSave } from 'react-icons/fi'
 
 function NamePicker(props) {
   const [name, setName] = useState('')
-  const [showName, setShowName] = useState(false)
+  const [editName, setEditName] = useState(true)
   const inputEl = useRef(null)
 
   function save(){
     inputEl.current.focus()
-    if(name && !showName) {
+    if(name && !editName) {
       props.onSave(name)
     }
-    setShowName(!showName)
+    setEditName(!editName)
   }
   return <div className="edit-username">
     <input value={name} ref={inputEl}
       className="name-input"
-      style={{display: showName ? 'none' : 'flex'}}
+      style={{display: editName ? 'none' : 'flex'}}
       onChange={e=> setName(e.target.value)}
       onKeyPress={e=> {
         if(e.key==='Enter') save()
       }}
     />
 
-    {showName && <div>{name}</div>}
+    {editName && <div>{name}</div>}
+    {name==='' && editName && <div>Set Username:</div>}
 
     <button onClick={save} className="name-button">
-      {showName ? <FiEdit /> : <FiSave />}
+      {editName ? <FiEdit /> : <FiSave />}
     </button>
   </div>
 }
