a3957f93555fe70d4de778b10701d17a96e33fb3
Ao Liu <aoliu@Aos-MacBook-Pro.local>
2020-01-29 09:43:33 -0800

    camera

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css'
 import {db, useDB} from './db'
 import NamePicker from './NamePicker'
 import { BrowserRouter, Route } from 'react-router-dom'
+import Camera from 'react-snap-pic'
+import {FiCamera} from 'react-icons/fi'
+import * as firebase from "firebase/app"
+import "firebase/storage"
 
 function App(){
   useEffect(()=>{
@@ -10,54 +14,84 @@ function App(){
     if(pathname.length<2) window.location.pathname='home'
   }, [])
   return <BrowserRouter>
-    <Route path="/:room" component={Room}/>
+    <Route path="/:room" component={Room} />
   </BrowserRouter>
 }
-
 
 function Room(props) {
   const {room} = props.match.params
   const [name, setName] = useState('')
+  const [showCamera, setShowCamera] = useState(false)
   const messages = useDB(room)
 
+  async function takePicture(img) {
+    setShowCamera(false)
+    const imgID = Math.random().toString(36).substring(7)
+    var storageRef = firebase.storage().ref()
+    var ref = storageRef.child(imgID + '.jpg')
+    await ref.putString(img, 'data_url')
+    db.send({ img: imgID, name, ts: new Date(), room })
+  }
+
   return <main>
+
+    {showCamera && <Camera takePicture={takePicture} />}
 
     <header>
       <div className="logo-wrap">
         <img className="logo"
           alt="logo"
-          src="https://c8.alamy.com/comp/W7BAC7/fruit-watermelon-logo-calligraphic-text-hand-drawn-vector-illustration-realistic-sketch-color-W7BAC7.jpg" 
+          src="https://images.coollogo.com/images/prism-large-green.png" 
         />
-        Chatter
+        chatterbox
       </div>
       <NamePicker onSave={setName} />
     </header>
 
     <div className="messages">
-      {messages.map((m,i)=>{
-        return <div key={i} className="message-wrap"
-          from={m.name===name?'me':'you'}>
-          <div className="message">
-            <div className="msg-name">{m.name}</div>
-            <div className="msg-text">{m.text}</div>
-          </div>
-        </div>
-      })}
+      {messages.map((m,i)=> <Message key={i} 
+        m={m} name={name} 
+      />)}
     </div>
 
-    <TextInput onSend={(text)=> {
-      db.send({
-        text, name, ts: new Date(), room
-      })
-    }} />
+    <TextInput 
+      showCamera={()=>setShowCamera(true)}
+      onSend={(text)=> {
+        db.send({
+          text, name, ts: new Date(), room
+        })
+      }}
+    />
     
   </main>
 }
 
+const bucket = 'https://firebasestorage.googleapis.com/v0/b/hcde-438.appspot.com/o/'
+const suffix = '.jpg?alt=media'
+
+function Message({m, name}){
+  return <div className="message-wrap"
+    from={m.name===name?'me':'you'}
+    onClick={()=>console.log(m)}>
+    <div className="message">
+      <div className="msg-name">{m.name}</div>
+      <div className="msg-text">
+        {m.text}
+        {m.img && <img src={bucket + m.img + suffix} alt="pic" />}
+      </div>
+    </div>
+  </div>
+}
+
 function TextInput(props){
   var [text, setText] = useState('') 
+
   // normal js comment
   return <div className="text-input-wrap">
+    <button onClick={props.showCamera} 
+      className = "camera">
+      <FiCamera />
+    </button>
     <input 
       value={text} 
       className="text-input"
@@ -81,23 +115,3 @@ function TextInput(props){
 }
 
 export default App
-
-/*
-import { BrowserRouter, Route } from "react-router-dom";
-function App() {
-  useEffect(()=>{
-    const {pathname} = window.location
-    if(pathname.length<2){
-      window.location.pathname = Math.random().toString(36).slice(7)
-    }
-  },[])
-  return (<BrowserRouter>
-    <Route path="/:room" component={Content} />
-  </BrowserRouter>)
-}
-*/
-
-
-
-
-// const {room} = props.match.params
